e84384ccc99cb2b6785ddba1a1d06d311bcb886b
KilliBruhh <killian.serluppens@gmail.com>
2024-11-18 10:49:00 +0100

    Added custom type with render item

--- line-bar-chart/src/App.tsx
+++ line-bar-chart/src/App.tsx
@@ -2,16 +2,14 @@
   import LineBarChart from './LineBarChart';
 
   const mockData = [
-    { name: 'Product A', value: 120 },
-    { name: 'Product B', value: 200 },
-    { name: 'Product C', value: 150 },
-    { name: 'Product D', value: 80 },
-    { name: 'Product E', value: 70 },
+    { name: '2022', value1: 150, value2: 30, value3: 200 },
+    { name: '2023', value1: 50, value2: 70, value3: 30 },
+    { name: '2024', value1: 100, value2: 130, value3: 300 },
   ];
 
   const App: React.FC = () => (
     <div>
-      <h1>Simple Bar Chart</h1>
+      <h1>Bar x Line Chart</h1>
       <LineBarChart data={mockData} title="Sales Data" />
     </div>
   );
